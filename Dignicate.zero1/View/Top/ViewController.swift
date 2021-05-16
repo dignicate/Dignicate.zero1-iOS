d8a8eaa66681590cd649bcf430de311c88dda3ea
Xinyiqi <dignicate@dignicate.com>
2021-05-17 00:49:42 +0900

    nib and ViewController

--- Dignicate.zero1/View/Top/ViewController.swift
+++ Dignicate.zero1/View/Top/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    private func setupUI() {
+
+    }
 
 }
 
