838c18dc45a692bd2e7a3785160b1600a3bc2f26
Xinyiqi <dignicate@dignicate.com>
2021-05-16 17:28:47 +0900

    Create the project.

---
+++ Dignicate.zero1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dignicate.zero1
+//
+//  Created by Xinyiqi on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
