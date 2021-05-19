38349d69d2223f1f8229e99d61196184c860f72f
Xinyiqi <dignicate@dignicate.com>
2021-05-19 23:35:56 +0900

    R.color

--- Dignicate.zero1/Resources.swift
+++ Dignicate.zero1/Resources.swift
@@ -8,6 +8,21 @@ struct R {
 
     private init() {}
 
+    enum color {
+        enum topView {
+            enum cell {
+                enum text {
+                    static let enabled = UIColor(named: "top_view_cell_text_enabled")
+                    static let disabled = UIColor(named: "top_view_cell_text_disabled")
+                }
+                enum background {
+                    static let enabled = UIColor(named: "top_view_cell_background_enabled")
+                    static let disabled = UIColor(named: "top_view_cell_background_disabled")
+                }
+            }
+        }
+    }
+
     enum nib {
         static var topViewTableCell: UINib {
             UINib(nibName: "TopViewTableCell", bundle: nil)
