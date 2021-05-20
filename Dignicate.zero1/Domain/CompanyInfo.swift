47ac20c5eb531d735916f661a0cda86633a0ec8a
Xinyiqi <dignicate@dignicate.com>
2021-05-20 22:00:46 +0900

    CompanyInfo

---
+++ Dignicate.zero1/Domain/CompanyInfo.swift
@@ -0,0 +1,15 @@
+//
+// Created by Xinyiqi on 2021/05/20.
+// Copyright (c) 2021 Dignicate,. All rights reserved.
+//
+
+import Foundation
+
+struct CompanyInfo {
+    let nameJP: String
+    let nameEN: String
+    let address: String
+    let foundationDate: String
+    let capital: Int
+    let numberOfEmployees: Int
+}
---
+++ Dignicate.zero1/Domain/CompanyInfoRepositoryProtocol.swift
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2021 Dignicate,. All rights reserved.
+//
+
+import RxSwift
+
+protocol CompanyInfoRepositoryProtocol {
+    func fetch()
+}
