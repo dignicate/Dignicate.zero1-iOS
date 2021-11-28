0d2cfc70b1f78a39ae08abaf56160313e688412f
Xinyiqi <dignicate@dignicate.com>
2021-11-28 12:11:37 +0900

    DZERO-55: View skeleton.

---
+++ Dignicate.zero1/View/04_IAP/OneTimeBillingViewController.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2021 Dignicate,. All rights reserved.
+//
+
+import UIKit
+
+final class OneTimeBillingViewController: UIViewController {
+
+
+}
---
+++ Dignicate.zero1/View/04_IAP/OneTimeBillingViewModel.swift
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2021 Dignicate,. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class OneTimeBillingViewModel {
+
+}
